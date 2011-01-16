a6e7c9c361e101f60b85881e369d3e3ec4efaa68
Ryan Voots <simcop2387@simcop2387.info>
2011-01-16 00:15:07 -0500

    found a good module

--- lib/Calendar.pm
+++ lib/Calendar.pm
@@ -1,5 +1,34 @@
 package Calendar;
 
+use strict;
+use warnings;
 
+use MediaConfig;
+
+use Data::Dumper;
+use Data::ICal;
+use LWP::UserAgent;
+use DateTime;
+use DateTime::Format::ISO8601;	
+
+#no need to recreate this all the time
+my $ua = LWP::UserAgent->new();
+
+sub get_calendar {
+	my $res = $ua->get(get_config("calendar"));
+	
+	if ($res->is_success()) {
+		return $res->content();
+	} else {
+		die "Couldn't get calendar: ".$res->status_line."\n".$res->content();
+	}
+}
+
+sub get_sched {
+	my $time = shift; # get the time we're asked about
+	my $ical = get_calendar();
+	my $calendar = Data::ICal->parse(data => $ical);
+	
+}
 
 1;
