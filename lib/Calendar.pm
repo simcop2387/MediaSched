adbf68da2ac98ad4ae092cfeeb5912ef4c363ca5
Ryan Voots <simcop2387@simcop2387.info>
2011-01-16 17:08:52 -0500

    almost ready!

--- lib/Calendar.pm
+++ lib/Calendar.pm
@@ -2,6 +2,7 @@ package Calendar;
 
 use strict;
 use warnings;
+use feature qw(switch);
 
 use MediaConfig;
 
@@ -30,12 +31,46 @@ sub get_sched {
 	my $ical = get_calendar();
 	my $calendar = Data::ICal->new(data => $ical);
 	
-	for my $event ($calendar->entries) {
+	die $calendar->error_message() unless $calendar;
+	# FIX THIS TO ACCEPT $TIME!
+	my $nowdt = DateTime->now();
+	
+	#collect all the events possible
+	my @events;
+	
+	for my $event (@{$calendar->entries}) {
 		next unless $event->ical_entry_type eq 'VEVENT';
-    	$event->{__dtstart} = $iso8601->parse_datetime( _prop($event, 'DTSTART') );
-    	$event->{__dtend}   = $iso8601->parse_datetime( _prop($event, 'DTEND') );
-    
-		print Dumper($event);
+    	my $dtstart = $iso8601->parse_datetime( _prop($event, 'DTSTART') );
+    	my $dtend   = $iso8601->parse_datetime( _prop($event, 'DTEND') );
+    	
+    	if (my $rrule = _prop($event, "rrule")) {
+    		print "REPEAT: $rrule :: "._prop($event, "description")."\n\n";
+    		
+    		my ($freq) = ($rrule =~ /FREQ=([^;]+)/g);
+    		
+    		if ($rrule =~ /UNTIL/) {
+    			my ($_until) = ($rrule =~ /UNTIL=([^;]+)/g);
+    			my $until = $iso8601->parse_datetime($_until);
+    			
+    			next if ($until < $nowdt); # we can throw this date out now if we're already past the date 
+    		}
+    		
+    		given ($freq) {
+    			when ("DAILY") {
+    			}
+    			when ("WEEKLY") {
+    				my %dow = (SU => 7, MO => 1, TU => 2, WE => 3, TH => 4, FR => 5, SA => 6);
+    				my @days = map {$dow{$_}} split(/,/, ($rrule =~ /BYDAY=([$;]+)/));
+    				
+    				for ($now
+    			}
+    		}
+    	} else {
+    		# range is [$dtstart, $dtend) that way a new event can start at $dtend without a conflict
+    		if ($nowdt >= $dtstart && $nowdt < $dtend) {
+    		  push @events, $event;
+    		}
+    	}
 	}
 }
 
