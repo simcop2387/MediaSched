5c5e99c8c652ca38d29efff48a96f3016872ade9
Ryan Voots <simcop2387@simcop2387.info>
2011-01-17 15:04:03 -0500

    updated example config, added the start of the player support
    removed the dzil stuff, don't care about it yet

--- lib/MediaConfig.pm
+++ lib/MediaConfig.pm
@@ -12,22 +12,30 @@ our @EXPORT = qw(get_config);
 use Data::Dumper;
 use YAML qw(LoadFile);
 
-my %options;
+my %options = (
+	defaultlist => ".", # play first storage directory
+	storage => $ENV{HOME}."/media/",
+	statefile => $ENV{HOME}."/.mediasched.state",
+);
+my $initconf;
 
 sub init_config {
 	#hardcoded location in current directory, might change this to something a little more flexible, or at least parse @ARGV for an alternate, not important right now
-	my $cf = LoadFile("config.yml");
+	my $cf= eval qw{LoadFile("config.yml");};
+	$cf //= {}; # make a blank default
 
 	#check some things we need
-	if (ref($cf) ne "HASH" || !$cf->{defaultlist} || !$cf->{statefile} || !$cf->{storage}) {
+	if (ref($cf) ne "HASH") {
 		die "Invalid Configuration: see README for an example";
 	}
 
-	%options = %$cf; # put it in the hash, looks nicer later
+	%options = (%options, %$cf); # put it in the hash, looks nicer later
+	print Dumper(\%options);
+	$initconf=1;
 }
 
 sub get_config {
-	init_config() unless (%options);
+	init_config() unless ($initconf);
 	
 	return $options{shift()}; # return what they wanted
 }
--- mythtest.pl
+++ mythtest.pl
@@ -1,57 +1,13 @@
 #!/usr/bin/perl
 
-#package Television;
+# This is a simple script, just around for starting the kernel and setting up the environment, will eventually pull config stuff from @ARGV
 
 use strict;
 use warnings;
 
 use lib './lib';
 
-use Mythtv;
 use POE;
-use Data::Dumper;
-
-use State;
-use Lists;
-use Log;
-
-my $ses = POE::Session->create(
-  package_states => 
-    [
-      "main"=> [ qw(_start playfile) ],
-	],
-	heap => {} );
+use Loop; # this does the real work
 
 POE::Kernel->run();
-
-sub _start
-{
-	my ($kernel, $heap) = @_[KERNEL, HEAP];
-
-	#my $file = Lists::getepisode();
-	#my $file = $kernel->call("Mythtv", "matchshow", "Family Guy");
-	#print Dumper $file;
-	$kernel->yield("playfile");	
-}
-
-sub playfile
-{
-	my ($kernel, $heap) = @_[KERNEL, HEAP];
-	
-	my $file = $kernel->call(Lists=>getepisode=>);
-	
-	debug 2, "Playing file :: $file";
-	savestate(); #make sure we save it, that way we don't have issues
-	die "wtf" unless (defined($file) && $file ne "");
-
-	if (-e "$file.edl")
-	{
-		system("mplayer", $file, "-edl", "$file.edl");
-	}
-	else
-	{
-		system("mplayer", "$file");
-	}
-
-	$kernel->yield(playfile=>);
-}
